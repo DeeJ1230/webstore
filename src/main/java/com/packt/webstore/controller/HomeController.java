0ba2db9f2e16a1816625746bf384c11de0a10f3e
Dan Jørgensen <djo@danskebank.dk>
2018-05-04 20:46:38 +0200

    environment variables removed

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.packt.webstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,19 +11,20 @@ public class HomeController {
 
 	@Autowired
 	private Environment env;
-	
-	@Value("${name}")
-	private String name;
 
-	@Value("${environment}")
-	private String environment;
-	
-	@Value("${dan}")
-	private int dan;
+	// @Value("${name}")
+	private String name = "dan";
+
+	// @Value("${environment}")
+	private String environment = "local";
+
+	// @Value("${dan}")
+	private int dan = 66;
 
 	@RequestMapping("/")
 	public String welcome(Model model) {
-//		model.addAttribute("greeting", "Welcome to Web store, " + env.getProperty("name") + "!");
+		// model.addAttribute("greeting", "Welcome to Web store, " +
+		// env.getProperty("name") + "!");
 		model.addAttribute("greeting", "Welcome to Web store, " + name + "! (" + dan + ")");
 		model.addAttribute("tagline", "The one and only");
 
