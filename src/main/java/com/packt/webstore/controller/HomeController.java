bb19eb3500ddad9dc717b915af9fa9a4acca1e54
Dan Jorgensen <dan,cph@gmail.com>
2019-06-02 11:38:03 +0200

    Added log file

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.packt.webstore.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private Environment env;
@@ -23,6 +26,9 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public String welcome(Model model) {
+
+	    logger.info("Welcome");
+
 		// model.addAttribute("greeting", "Welcome to Web store, " +
 		// env.getProperty("name") + "!");
 		model.addAttribute("greeting", "Welcome to Web store, " + name + "! (" + dan + ")");
